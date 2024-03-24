5e320a5d07a6748af7be562b65bdf7f4e6c31cfb
Yousef Ismail <yousef2142003@gmail.com>
2024-03-24 16:11:54 +0200

    Update Portfolio Structure

---
+++ JavaScript/script.js
@@ -0,0 +1,48 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links .classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+    const mediaItems = document.querySelectorAll(".media-item");
+    let currentIndex = 0;
+
+    function showMediaItem(index) {
+        mediaItems.forEach((item, i) => {
+            if (i === index) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    }
+
+    prevButton.addEventListener("click", function() {
+        currentIndex = (currentIndex === 0) ? mediaItems.length - 1 : currentIndex - 1;
+        showMediaItem(currentIndex);
+    });
+
+    nextButton.addEventListener("click", function() {
+        currentIndex = (currentIndex === mediaItems.length - 1) ? 0 : currentIndex + 1;
+        showMediaItem(currentIndex);
+    });
+});
